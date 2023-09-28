97d154dd5625ce91721974eb30f849add3d3ae87
pyshnenko <pyshnenko94@yandex.ru>
2023-09-28 15:36:07 +0300

    uploader

--- uploader.js
+++ uploader.js
@@ -2,7 +2,7 @@ const express = require("express");
 const multer  = require("multer");
 const cors = require('cors');
 const fs = require('fs');
-process.title='APIServer';
+process.title='APIServerGF';
 const app = express();
 
 app.use(cors())
